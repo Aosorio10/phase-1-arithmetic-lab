7c21566202d25958ec639e3b002db706d3a46415
Alejandro Osorio <avot3110@hotmail.com>
2023-03-05 17:01:28 -0500

    Arithmetich-lab

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+function multiply(num1, num2){
+    return num1 * num2;
+    result = 62;
+}
+var multiply = (31 * 2);
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+
+function random(){
+    let random = (Math.random(4) * 10) + 1;
+    return randon;
+    
+  }
+  var random = ((2) * 13 ) + 3;
+
+  function mod(num3, num4){
+    return num3 % num4;
+    result = 4;
+  }
+  let num3 = 40;
+  let num4 = 6;
+  var mod = (40 % 6);
+
+  function max(){
+    return max
+    result = 20;
+  }
+  var max = (1, 3, 5, 6, 20);
